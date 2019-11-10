69ee7a365ae9a37852391d0a9a87f9473322d2c9
SilkCrown <sithtriumvirate@gmail.com>
2019-11-10 15:42:41 -0800

    Entity, Componenet, Systm

--- src/ship.js
+++ src/ship.js
@@ -1,1 +1,29 @@
-//commentgit
+class shipAttributes {
+
+}
+
+class shipInventory {
+    constructor ( { compartmentArray, componentArray} ) {
+
+    }
+}
+
+class Item {
+    constructor ( { itemCharacteristics } ) {
+        
+
+    create() {}
+}
+    itemCreator( {Item, Creator} ) {
+        if (Item.characteristic.rarity == 0) {
+            
+        }
+}
+}
+
+let bagOfTea = {
+    weight: 50,
+    stackSize: 7,
+    value: 100,
+    rarity: 0
+}
